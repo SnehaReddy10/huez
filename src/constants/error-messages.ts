54b404dd7c86c6d12832e4da54d17de5ef0bb4a4
Sneha Reddy <snehareddy9178@gmail.com>
2024-11-17 20:28:58 +0530

    feat: add create cart controller

--- src/constants/error-messages.ts
+++ src/constants/error-messages.ts
@@ -10,6 +10,24 @@ export const ErrorMessages = {
     UserNotFound: 'User not found. Please check your credentials.',
     InvalidCredentials: 'Invalid email or password.',
     SignInSuccessful: 'Sign-in successful.',
+    Unauthorized: 'User is not authenticated.',
+    Forbidden: 'You do not have the required permissions.',
+    ForbiddenMerchantAccess: 'You must be a merchant to access this resource.',
+    ForbiddenAdminAccess: 'You must be an admin to access this resource.',
+  },
+  Merchant: {
+    NotFound: 'Merchant not found',
+    NotAuthorized: 'Merchant is not authorized to create a restaurant',
+  },
+  Restaurant: {
+    NotFound: 'Restaurant not found.',
+  },
+  MenuItem: {
+    NotFound: 'Menu Item Not Found',
+  },
+  Cart: {
+    CartAlreadyExists: 'Cart Already Exists',
+    NotFound: 'Cart Not Found',
   },
   Server: {
     InternalServerError: 'Internal Server error',
--- src/constants/status-codes.ts
+++ src/constants/status-codes.ts
@@ -1,10 +1,11 @@
 export const StatusCodes = {
-  Success: 200,
+  OK: 200,
   Created: 201,
   NoContent: 204,
   BadRequest: 400,
   Unauthorized: 401,
   Forbidden: 403,
+  NotFound: 404,
   MethodNotAllowed: 405,
   Conflict: 409,
   InternalServerError: 500,
